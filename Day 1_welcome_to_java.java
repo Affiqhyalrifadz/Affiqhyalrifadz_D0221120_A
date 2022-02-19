fdeb4dd1e63f25e5d4b9b8cff9f3e10e32701b99
Affiqhyalrifadz <99630265+Affiqhyalrifadz@users.noreply.github.com>
2022-02-19 14:40:10 +0800

    Day1_,welcome_to_java.java

---
+++ Day 1_welcome_to_java.java
@@ -0,0 +1,8 @@
+public class day 1_welcome_to_java.java,{
+     public static void main(string[] args) {
+             System.out.println ("hello, word,");
+             System.uot.println ("hello, word,");
+
+}
+
+}
